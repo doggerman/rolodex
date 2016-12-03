c053eed6758a0b091eb91892e67c4cb68d9fbbe4
idk <idk@bestallning@idrottskoll.se>
2016-12-03 01:45:49 +0100

    updated structure

--- app/functions.php
+++ app/functions.php
@@ -35,31 +35,32 @@ return;
 
 function loopDisplay($db, $query){
     print '<table class="display">';
-    print '<th>Id</th>';
-    print '<th>Email</th>';
-    print '<th>Phone</th>';
-    print '<th>Firstname</th>';
-    print '<th>Lastname</th>';
-    print '<th>Make</th>';
-    print '<th>Model</th>';
-    print '<th>Typo</th>';
-    print '<th>Handle</th>';
-    print '<th>ProfileUrl</th>';
-    print '</tr>';
+      print '</tr>';
+        print '<th>Id</th>';
+        print '<th>Email</th>';
+        print '<th>Phone</th>';
+        print '<th>Firstname</th>';
+        print '<th>Lastname</th>';
+        print '<th>Make</th>';
+        print '<th>Model</th>';
+        print '<th>Typo</th>';
+        print '<th>Handle</th>';
+        print '<th>ProfileUrl</th>';
+      print '</tr>';
 
   foreach (selectMultiInDB($db, $query) as $result) {
-    print '<tr>';
-    print '<td>'.$result['id'].'</td>';
-    print '<td>'.$result['email'].'</td>';
-    print '<td>'.$result['phone'].'</td>';
-    print '<td>'.$result['firstName'].'</td>';
-    print '<td>'.$result['lastName'].'</td>';
-    print '<td>'.$result['make'].'</td>';
-    print '<td>'.$result['model'].'</td>';
-    print '<td>'.$result['typo'].'</td>';
-    print '<td>'.$result['handle'].'</td>';
-    print '<td>'.$result['profileUrl'].'</td>';
-    print '</tr>';
+      print '<tr>';
+        print '<td>'.$result['id'].'</td>';
+        print '<td>'.$result['email'].'</td>';
+        print '<td>'.$result['phone'].'</td>';
+        print '<td>'.$result['firstName'].'</td>';
+        print '<td>'.$result['lastName'].'</td>';
+        print '<td>'.$result['make'].'</td>';
+        print '<td>'.$result['model'].'</td>';
+        print '<td>'.$result['typo'].'</td>';
+        print '<td>'.$result['handle'].'</td>';
+        print '<td>'.$result['profileUrl'].'</td>';
+      print '</tr>';
   }
     print '</table>';
 return;
